72d89e8de6f6e40d13084310e4725e259dbebb61
Punit239 <punit239@gmail.com>
2013-12-02 17:17:50 -0800

    Adding latest SentimentalAnalysis.java file

--- src/main/java/edu/sjsu/cmpe/bigdata/SentimentAnalysis.java
+++ src/main/java/edu/sjsu/cmpe/bigdata/SentimentAnalysis.java
@@ -1,4 +1,4 @@
-package edu.sjsu.cmpe.bigdata;
+package tengen;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -21,7 +21,7 @@ import com.mongodb.MongoClient;
 
 public class SentimentAnalysis {
 
-	public String executePost(String targetURL, String urlParameters) {
+	public String excutePost(String targetURL, String urlParameters) {
 		URL url;
 		HttpURLConnection connection = null;
 		try {
@@ -40,7 +40,7 @@ public class SentimentAnalysis {
 			connection.setDoInput(true);
 			connection.setDoOutput(true);
 
-			// Send Request
+			// Send request
 			DataOutputStream wr = new DataOutputStream(
 					connection.getOutputStream());
 			wr.writeBytes(urlParameters);
@@ -60,6 +60,7 @@ public class SentimentAnalysis {
 			return response.toString();
 
 		} catch (Exception e) {
+
 			//e.printStackTrace();
 			return "This review is not evaluated";
 
@@ -69,5 +70,79 @@ public class SentimentAnalysis {
 				connection.disconnect();
 			}
 		}
+	}
+	
+	public static void main(String args[]) throws UnknownHostException
+	{
+		MongoClient client = new MongoClient();
+		DB courseDB = client.getDB("codeassassins");
+		DBCollection collection = courseDB.getCollection("bigdata");
+		
+		// Query for fetching reviews for a particular restaurant
+		DBObject query = new BasicDBObject("type","review").append("business_id", "VFslQjSgrw4Mu5_Q1xk1KQ");
+		//DBObject query = new BasicDBObject("type","review").append("business_id", "LjOIxpH-89S18WI1ktmPBQ");
+		
+		DBCursor cursor = collection.find(query);
+
+		SentimentalAnalysis builder = new SentimentalAnalysis();
+		
+		// URL for using sentigem API for sentimental analysis
+		String baseurl = "https://api.sentigem.com/external/get-sentiment?api-key=c7b7029934a1159ae3e2a9c3f80d3f99PiXQg6kuBN_0dp2Z8E5lrWTD4-oAqJbY&text=";
+				
+		String param = "";
+		String response = "";
+		int neutral=0, negative=0, positive=0,notEval=0;
+		
+		
+		while(cursor.hasNext()){
+			
+			DBObject cur = cursor.next();
+			String userReview = (String) cur.get("text");
+			try {
+				String encodedUserReview = java.net.URLEncoder.encode(userReview, "ISO-8859-1");
+
+			String finalurl = baseurl+encodedUserReview;
+			response = builder.excutePost(finalurl,param);
+			System.out.print(response+"\n");
+			if (response.contains("neutral")){
+				neutral++;
+			}
+			else if (response.contains("positive")){
+				positive++;
+			}
+			else if (response.contains("negative")){
+				negative++;
+			}
+			else if (response.equalsIgnoreCase("This review is not evaluated")){
+				notEval++;
+			}
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		
+		DBCollection collection1 = courseDB.getCollection("analysis");
+
+		//String b_id = "VFslQjSgrw4Mu5_Q1xk1KQ";
+		String b_id = "LjOIxpH-89S18WI1ktmPBQ";
+		
+		DBObject doc = new BasicDBObject()
+							.append("business_id", b_id)
+							.append("positive", positive)
+							.append("negative", negative)
+							.append("neutral", neutral)
+							.append("notEval", notEval)
+							.append("date", new Date())
+							;
+
+		collection1.insert(doc);
+
+		
+		System.out.println("Number of neutral reviews       : "+ neutral);
+		System.out.println("Number of positive reviews      : "+ positive);
+		System.out.println("Number of negative reviews      : "+ negative);
+		System.out.println("Number of reviews not evaluated : "+ notEval);
+
 	}
 }
