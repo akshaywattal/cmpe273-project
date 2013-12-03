e8f8e0617f1e536791b89e28b62261d0719eca45
akshaywattal <akshay.wattal@gmail.com>
2013-12-02 22:23:16 -0800

    Added DTO for Sentiment Analysis

--- src/main/java/edu/sjsu/cmpe/bigdata/dto/SentimentAnalysisDto.java
+++ src/main/java/edu/sjsu/cmpe/bigdata/dto/SentimentAnalysisDto.java
@@ -15,8 +15,10 @@ import com.mongodb.MongoClient;
 
 public class SentimentAnalysisDto {
 	
+	private String response;
+	
 	public String getSentiment() throws UnknownHostException {
-		
+		 
 		// Creating connection with MongoDB
 		MongoClient client = new MongoClient();
 		DB courseDB = client.getDB("bigdata");
@@ -46,7 +48,12 @@ public class SentimentAnalysisDto {
 		System.out.println("Number of negative reviews      : "+ negative);
 		System.out.println("Number of reviews not evaluated : "+ notEval);
 		
-		//NOTE: Fix below to return data in json format
-		return "";
+		// Creating String in json format
+		response = "{" + "\"neutral\":" + neutral 
+					   + ",\"positive\":" + positive
+					   + ",\"negative\":" + negative 
+					   + ",\"notEval\":" + notEval + "}";
+		
+		return response;
 		}
 	}
